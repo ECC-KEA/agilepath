10f7e76015796ea9ec2832c48bf841f2d6ec7d17
emilvn <emilvnielsen@hotmail.com>
2025-03-29 14:30:23 +0100

    init Poppins font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import LoadingSpinner from "./components/generic/loading/LoadingSpinner";
 import Logo from "./components/generic/Logo";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen space-x-2 text-2xl">
-      <Logo size={50} />
-      <div className="text-3xl">AgilePath</div>
+    <div className="h-screen flex flex-col items-center justify-center space-y-4">
+      <div className="flex items-center justify-center space-x-2 text-2xl">
+        <Logo size={50} />
+        <div className="text-3xl">AgilePath</div>
+      </div>
+      <LoadingSpinner size={25} />
     </div>
   );
 }
