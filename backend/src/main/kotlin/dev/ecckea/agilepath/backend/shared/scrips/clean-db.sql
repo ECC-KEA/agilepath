fadb338f0ee5fa8998225863d2bf474a639e2323
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-04-08 22:51:13 +0200

    Add SQL scripts for database cleanup and dummy data seeding

---
+++ backend/src/main/kotlin/dev/ecckea/agilepath/backend/shared/scrips/clean-db.sql
@@ -0,0 +1,3 @@
+-- This script is used to clean the database and create a new one and should ONLY be used manually in local development.
+DROP DATABASE IF EXISTS agilepath;
+CREATE DATABASE agilepath;
