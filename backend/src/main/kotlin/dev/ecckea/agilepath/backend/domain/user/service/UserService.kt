2ecfba19352161ea24756608ff2ead09ffff9e9c
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-04-08 22:55:32 +0200

    Refactor UserService to use id instead of clerkId and make getOrCreate function suspendable with caching

--- backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/user/service/UserService.kt
+++ backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/user/service/UserService.kt
@@ -1,23 +1,36 @@
 package dev.ecckea.agilepath.backend.domain.user.service
 
-import dev.ecckea.agilepath.backend.domain.user.repository.entity.toModel
 import dev.ecckea.agilepath.backend.domain.user.model.User
 import dev.ecckea.agilepath.backend.domain.user.repository.UserRepository
+import dev.ecckea.agilepath.backend.domain.user.repository.entity.toModel
+import dev.ecckea.agilepath.backend.shared.coroutines.withIO
 import dev.ecckea.agilepath.backend.shared.exceptions.ResourceNotFoundException
+import dev.ecckea.agilepath.backend.shared.logging.Logged
 import dev.ecckea.agilepath.backend.shared.security.UserPrincipal
 import dev.ecckea.agilepath.backend.shared.security.toEntity
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
-    private val userRepository: UserRepository
-) {
-    fun getOrCreate(principal: UserPrincipal): User {
-        val exists = userRepository.existsByClerkId(principal.clerkId)
+    private val userRepository: UserRepository,
+) : Logged() {
+    /**
+     * Returns the user matching the id of the principal. If the user does not exist, it will be created.
+     * Caches the result using the id as key.
+     *
+     * @param principal the principal to get the user for
+     * @return the user matching the id of the principal
+     * @throws ResourceNotFoundException if the user does not exist
+     */
+    @Cacheable("users", key = "#principal.id")
+    suspend fun getOrCreate(principal: UserPrincipal): User = withIO {
+        val exists = userRepository.existsById(principal.id)
         if (!exists) {
+            log.info("User with id ${principal.id} does not exist, creating it")
             userRepository.save(principal.toEntity())
         }
-        val user = userRepository.findByClerkId(principal.clerkId) ?: throw ResourceNotFoundException("User not found")
-        return user.toModel()
+        userRepository.findOneById(principal.id)?.toModel() ?: throw ResourceNotFoundException("User with id ${principal.id} not found")
     }
+
 }
