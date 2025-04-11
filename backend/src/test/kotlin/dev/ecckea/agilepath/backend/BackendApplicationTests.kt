4e6284801b252a87e4a57673b73874bae9f12ef6
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-04-11 17:41:32 +0200

    #4 configure test environment with active profile and test properties
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

--- backend/src/test/kotlin/dev/ecckea/agilepath/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/dev/ecckea/agilepath/backend/BackendApplicationTests.kt
@@ -2,8 +2,12 @@ package dev.ecckea.agilepath.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = ["CLERK_ISSUER=https://fake-issuer.dev"])
 class BackendApplicationTests {
 
     @Test
