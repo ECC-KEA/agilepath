0ae829872fb31d7c99fbb24d904c014c1961d721
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-04-11 17:39:46 +0200

    #4 add development dependencies and configure bootJar task
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -61,11 +61,15 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
 
+    // --- Development ---
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     // --- Tests ---
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.mockk:mockk:$mockkVersion")
+    runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -85,4 +89,8 @@ allOpen {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.bootJar {
+    archiveFileName.set("app.jar")
 }
