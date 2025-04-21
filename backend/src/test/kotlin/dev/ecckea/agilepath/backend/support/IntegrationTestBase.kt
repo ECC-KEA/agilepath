1a1147cdfaf789866d9edd79faed504a17bf911a
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-04-21 09:50:59 +0200

    #13 Add IntegrationTestBase for setting up mock MVC tests
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

---
+++ backend/src/test/kotlin/dev/ecckea/agilepath/backend/support/IntegrationTestBase.kt
@@ -0,0 +1,26 @@
+package dev.ecckea.agilepath.backend.support
+
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+abstract class IntegrationTestBase {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    protected lateinit var webTestClient: WebTestClient
+
+    @BeforeEach
+    fun setUpClient() {
+        webTestClient = MockMvcWebTestClient.bindTo(mockMvc).build()
+    }
+}
