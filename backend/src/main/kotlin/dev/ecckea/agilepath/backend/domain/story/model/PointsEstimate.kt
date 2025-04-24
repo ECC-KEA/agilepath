94d4ff3b37f94a33a081ff875aa993fda3eab4fa
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-04-24 16:25:25 +0200

    #11 Add enums for PointEstimate and TshirtEstimate to represent estimation values
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

---
+++ backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/story/model/PointsEstimate.kt
@@ -0,0 +1,11 @@
+package dev.ecckea.agilepath.backend.domain.story.model
+
+enum class PointEstimate(val points: Int) {
+    POINT_1(1),
+    POINT_2(2),
+    POINT_3(3),
+    POINT_5(5),
+    POINT_8(8),
+    POINT_13(13),
+    POINT_21(21)
+}
