62e61bc25d3e9e69b9371cf22b630db8fd35feff
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-04-28 20:56:29 +0200

    #11 Add RepositoryContext and repository extension functions for JPA repositories
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

---
+++ backend/src/main/kotlin/dev/ecckea/agilepath/backend/shared/context/repository/RepositoryContext.kt
@@ -0,0 +1,24 @@
+package dev.ecckea.agilepath.backend.shared.context.repository
+
+import dev.ecckea.agilepath.backend.domain.column.repository.SprintColumnRepository
+import dev.ecckea.agilepath.backend.domain.project.repository.ProjectRepository
+import dev.ecckea.agilepath.backend.domain.sprint.repository.SprintRepository
+import dev.ecckea.agilepath.backend.domain.story.repository.*
+import dev.ecckea.agilepath.backend.domain.user.repository.UserRepository
+import org.springframework.stereotype.Component
+
+@Component
+class RepositoryContext(
+    val user: UserRepository,
+    val project: ProjectRepository,
+    val sprint: SprintRepository,
+    val sprintColumn: SprintColumnRepository,
+    val story: StoryRepository,
+    val task: TaskRepository,
+    val taskAssinee: TaskAssigneeRepository,
+    val subtask: SubtaskRepository,
+    val comment: CommentRepository,
+
+    )
+
+
