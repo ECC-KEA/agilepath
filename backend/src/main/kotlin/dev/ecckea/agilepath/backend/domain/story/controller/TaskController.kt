d6263b55b234436683ce2fd37eb2854bc2c396a5
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-05-06 14:19:38 +0200

    #28 Add validation to TaskRequest in TaskController for improved data integrity
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

--- backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/story/controller/TaskController.kt
+++ backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/story/controller/TaskController.kt
@@ -11,12 +11,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 @RequestMapping("/tasks")
+@Validated
 @Tag(name = "Tasks", description = "Endpoints related to Task management")
 class TaskController(
     private val taskApplication: TaskApplication
@@ -40,7 +43,7 @@ class TaskController(
         ]
     )
     @PostMapping
-    fun createTask(@RequestBody taskRequest: TaskRequest): TaskResponse {
+    fun createTask(@Valid @RequestBody taskRequest: TaskRequest): TaskResponse {
         log.info("POST /tasks - Create task")
         return taskApplication.createTask(taskRequest.toModel()).toDTO()
     }
@@ -86,7 +89,7 @@ class TaskController(
         ]
     )
     @PutMapping("/{id}")
-    fun updateTask(@PathVariable id: UUID, @RequestBody taskRequest: TaskRequest): TaskResponse {
+    fun updateTask(@PathVariable id: UUID, @Valid @RequestBody taskRequest: TaskRequest): TaskResponse {
         log.info("PUT /tasks/$id - Update task")
         return taskApplication.updateTask(id, taskRequest.toModel()).toDTO()
     }
