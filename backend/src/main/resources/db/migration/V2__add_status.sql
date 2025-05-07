f1a2a67171887d2ea0ee6d8b92ecc97abf4b212e
emilvn <emilvnielsen@hotmail.com>
2025-05-07 12:20:48 +0200

    #30 attempt to fix h2 syntax err

--- backend/src/main/resources/db/migration/V2__add_status.sql
+++ backend/src/main/resources/db/migration/V2__add_status.sql
@@ -1,3 +1,5 @@
 ALTER TABLE sprint_columns
-    DROP CONSTRAINT sprint_columns_status_check,
+DROP CONSTRAINT sprint_columns_status_check;
+
+ALTER TABLE sprint_columns
     ADD CONSTRAINT sprint_columns_status_check CHECK (status IN ('TODO', 'IN_PROGRESS', 'DONE', 'ARCHIVED'));
