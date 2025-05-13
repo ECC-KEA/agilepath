7cec69c895d403eb878bbd2471541588f4bf37c4
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-05-13 22:44:43 +0200

    #28 Enhance validation annotations and validators with detailed documentation
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

--- backend/src/main/kotlin/dev/ecckea/agilepath/backend/shared/validation/validators/NoHtmlValidator.kt
+++ backend/src/main/kotlin/dev/ecckea/agilepath/backend/shared/validation/validators/NoHtmlValidator.kt
@@ -4,7 +4,21 @@ import dev.ecckea.agilepath.backend.shared.validation.annotations.NoHtml
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
 
+/**
+ * Validator to ensure that a string does not contain HTML tags.
+ * This validator is used in conjunction with the `NoHtml` annotation.
+ *
+ * Implements the `ConstraintValidator` interface for the `NoHtml` annotation.
+ */
 class NoHtmlValidator : ConstraintValidator<NoHtml, String> {
+
+    /**
+     * Validates whether the given string value does not contain HTML tags.
+     *
+     * @param value The string value to validate.
+     * @param context The `ConstraintValidatorContext` to build custom constraint violations.
+     * @return `true` if the value is null or does not contain HTML tags, `false` otherwise.
+     */
     override fun isValid(value: String?, context: ConstraintValidatorContext): Boolean {
         return value == null || !value.contains(Regex("<[^>]++>"))
     }
