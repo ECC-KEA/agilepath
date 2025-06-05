49bded618c124673174e35fdbc3cd1e8c09b07a4
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-06-05 02:49:00 +0200

    #46 add query to find projects by sprint ID
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

--- backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/project/repository/ProjectRepository.kt
+++ backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/project/repository/ProjectRepository.kt
@@ -1,11 +1,10 @@
 package dev.ecckea.agilepath.backend.domain.project.repository
 
 import dev.ecckea.agilepath.backend.domain.project.repository.entity.ProjectEntity
-import dev.ecckea.agilepath.backend.domain.user.model.User
 import dev.ecckea.agilepath.backend.domain.user.repository.entity.UserEntity
-import dev.ecckea.agilepath.backend.shared.security.UserPrincipal
-import dev.ecckea.agilepath.backend.shared.security.toEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -15,4 +14,14 @@ interface ProjectRepository : JpaRepository<ProjectEntity, UUID> {
     fun findByName(name: String): ProjectEntity?
     fun existsByName(name: String): Boolean
     fun findAllByCreatedBy(user: UserEntity): List<ProjectEntity>
+
+    @Query(
+        value = """
+            SELECT p.* FROM projects p 
+            JOIN sprints s ON s.project_id = p.id 
+            WHERE s.id = :sprintId
+        """,
+        nativeQuery = true
+    )
+    fun findBySprintId(@Param("sprintId") sprintId: UUID): ProjectEntity?
 }
