e45f3027e6a0ea62ae4e013fccd3449bed33300d
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-06-05 02:24:32 +0200

    #46 add queries to retrieve and count tasks for a specific sprint
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

--- backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/story/repository/TaskRepository.kt
+++ backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/story/repository/TaskRepository.kt
@@ -1,9 +1,10 @@
 package dev.ecckea.agilepath.backend.domain.story.repository
 
-import dev.ecckea.agilepath.backend.domain.column.model.SprintColumn
 import dev.ecckea.agilepath.backend.domain.column.repository.entity.SprintColumnEntity
 import dev.ecckea.agilepath.backend.domain.story.repository.entity.TaskEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -12,4 +13,44 @@ interface TaskRepository : JpaRepository<TaskEntity, UUID> {
     fun findByStoryId(storyId: UUID): List<TaskEntity>
     fun findOneById(id: UUID): TaskEntity?
     fun findBySprintColumn(sprintColumn: SprintColumnEntity): List<TaskEntity>
+
+    @Query(
+        value = """
+        SELECT * FROM tasks t
+        JOIN sprint_columns sc ON t.sprint_column_id = sc.id
+        WHERE sc.sprint_id = :sprintId AND sc.status = 'DONE'
+    """,
+        nativeQuery = true
+    )
+    fun findCompletedTasksForSprint(@Param("sprintId") sprintId: UUID): List<TaskEntity>
+
+    @Query(
+        value = """
+        SELECT COUNT(*) FROM tasks t
+        JOIN sprint_columns sc ON t.sprint_column_id = sc.id
+        WHERE sc.sprint_id = :sprintId AND sc.status = 'DONE'
+    """,
+        nativeQuery = true
+    )
+    fun countCompletedTasksForSprint(@Param("sprintId") sprintId: UUID): Int
+
+    @Query(
+        value = """
+        SELECT * FROM tasks t
+        JOIN sprint_columns sc ON t.sprint_column_id = sc.id
+        WHERE sc.sprint_id = :sprintId
+    """,
+        nativeQuery = true
+    )
+    fun findAllTasksForSprint(@Param("sprintId") sprintId: UUID): List<TaskEntity>
+
+    @Query(
+        value = """
+        SELECT COUNT(*) FROM tasks t
+        JOIN sprint_columns sc ON t.sprint_column_id = sc.id
+        WHERE sc.sprint_id = :sprintId
+    """,
+        nativeQuery = true
+    )
+    fun countAllTasksForSprint(@Param("sprintId") sprintId: UUID): Int
 }
