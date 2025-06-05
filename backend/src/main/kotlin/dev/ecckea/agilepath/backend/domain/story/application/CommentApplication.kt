d1b8a34822c6ec8f7139cab8c29f371a0b1d3426
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-06-05 02:48:12 +0200

    #46 add event logging for comment creation in tasks and stories
    Signed-off-by: Chris Skov Sørensen <chso0002@stud.kea.dk>

--- backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/story/application/CommentApplication.kt
+++ backend/src/main/kotlin/dev/ecckea/agilepath/backend/domain/story/application/CommentApplication.kt
@@ -2,15 +2,23 @@ package dev.ecckea.agilepath.backend.domain.story.application
 
 import dev.ecckea.agilepath.backend.domain.story.model.Comment
 import dev.ecckea.agilepath.backend.domain.story.model.NewComment
+import dev.ecckea.agilepath.backend.domain.story.model.StoryEventType
+import dev.ecckea.agilepath.backend.domain.story.model.TaskEventType
 import dev.ecckea.agilepath.backend.domain.story.service.CommentService
+import dev.ecckea.agilepath.backend.domain.user.service.UserService
 import dev.ecckea.agilepath.backend.shared.logging.Logged
+import dev.ecckea.agilepath.backend.shared.logging.events.StoryEventLogger
+import dev.ecckea.agilepath.backend.shared.logging.events.TaskEventLogger
 import dev.ecckea.agilepath.backend.shared.security.currentUser
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class CommentApplication(
-    private val commentService: CommentService
+    private val commentService: CommentService,
+    private val userService: UserService,
+    private val taskEventLogger: TaskEventLogger,
+    private val storyEventLogger: StoryEventLogger
 ) : Logged() {
 
     fun getComment(id: UUID): Comment {
@@ -30,7 +38,32 @@ class CommentApplication(
 
     fun createComment(newComment: NewComment): Comment {
         log.info("Creating comment")
-        return commentService.createComment(newComment)
+        val createdComment = commentService.createComment(newComment)
+        val user = userService.getById(newComment.createdBy)
+
+        when {
+            newComment.taskId != null -> {
+                taskEventLogger.logEvent(
+                    entityId = newComment.taskId,
+                    eventType = TaskEventType.COMMENT_ADDED,
+                    triggeredBy = user,
+                    oldValue = null,
+                    newValue = createdComment.content
+                )
+            }
+
+            newComment.storyId != null -> {
+                storyEventLogger.logEvent(
+                    entityId = newComment.storyId,
+                    eventType = StoryEventType.COMMENT_ADDED,
+                    triggeredBy = user,
+                    oldValue = null,
+                    newValue = createdComment.content
+                )
+            }
+        }
+
+        return createdComment
     }
 
     fun updateComment(id: UUID, newComment: NewComment): Comment {
